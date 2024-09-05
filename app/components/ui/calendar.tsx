750637b95e56091febe221af300692a4b53912c1
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2024-09-05 23:02:15 +0530

    alignment

--- app/components/ui/calendar.tsx
+++ app/components/ui/calendar.tsx
@@ -33,9 +33,9 @@ function Calendar({
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
-          "text-muted-foreground rounded-md w-8 lg:w-[2.75em] sxl:w-8 xl:w-[39px] xxl:w-11 2xl:w-[52px] 3xl:w-14 font-normal text-[0.7rem]",
+          "text-muted-foreground rounded-md w-8 lg:w-[2.75em] sxl:w-8 xl:w-[39px] xxl:w-11 2xl:w-[48px] 3xl:w-14 font-normal text-[0.7rem]",
         row: "flex w-full mt-2",
-        cell: "h-7 xl:h-8 2xl:h-10 w-8 lg:w-[2.1em] sxl:w-8 xl:w-[39px] xxl:w-11 2xl:w-[52px] 3xl:w-14 text-center text-[0.9rem] p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "h-7 xl:h-8 2xl:h-10 w-8 lg:w-[2.1em] sxl:w-8 xl:w-[39px] xxl:w-11 2xl:w-[48px] 3xl:w-14 text-center text-[0.9rem] p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-8 2xl:h-10 w-8 2xl:w-10 p-0 rounded-full font-normal aria-selected:opacity-100"
